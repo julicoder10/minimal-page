4c916efcad5c28334b985a48f30cbf3b3b2c925a
julicoder10 <julicoder10@gmail.com>
2022-03-10 23:55:41 -0300

    Me falta el modo responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InfoComponent } from './components/info/info.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    InfoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
